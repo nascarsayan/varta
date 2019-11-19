2e7dbe0877addb546eca42f2ac19861180ce5d59
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2019-11-20 03:47:29 +0530

    Cleaned comments

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const Twitter = require('twitter')
 const express = require('express')
-// const cors = require('cors')
-// const moment = require('moment')
 const queryString = require('query-string')
 require('dotenv').config()
 const Database = require('./db')
@@ -28,7 +26,6 @@ const query = async (term, max_id = null) => {
     if (max_id) {
       qobj = { ...qobj, max_id }
     }
-    // console.log(qobj)
     let tweets = await client.get('search/tweets', qobj)
     tweets.statuses = tweets.statuses.map(status => ({
       ...status,
@@ -36,7 +33,6 @@ const query = async (term, max_id = null) => {
       date: new Date(status.created_at),
       created_on: new Date()
     }))
-    // console.log(tweets.statuses[0])
     return tweets
   } catch (err) {
     console.log(err)
@@ -74,43 +70,8 @@ app.get('/api/query/recent', async (req, res) => {
     console.log(err)
   }
   res.header("Access-Control-Allow-Origin", "*")
-  // console.log(tres.length)
   res.send(tres)
 })
-
-// app.get('/api/query/max', async (req, res) => {
-//   const term = req.params('term')
-//   const query = async (term, start = null, end = null) => {
-//     // console.log(`place:b850c1bfd38f30e0 ${term}${(start) ? ` since:${start}` : ''}${(end) ? ` until:${end}` : ''}`);
-//     const tweets = await client.get('search/tweets', {
-//       q: `place:b850c1bfd38f30e0 ${term}${(start) ? ` since:${start}` : ''}${(end) ? ` until:${end}` : ''}`,
-//       count: 100
-//     })
-//     return tweets.statuses.map(t => {
-//       let url = null
-//       try {
-//         url = t.entities.urls[0].url
-//       } catch {
-//         url = 'https://twitter.com/'
-//       }
-//       return {
-//         id: t.id, place: t.place.name, coord: t.place.bounding_box.coordinates, text: t.text, url, raw: t
-//       }
-//     })
-//   }
-//   let range = n => Array.from(Array(n).keys())
-//   let result = [], old = 4
-//   let dates = range(old).map(v => ({
-//     start: moment().subtract((v + 1) * 7, 'd').format('YYYY-MM-DD'),
-//     end: moment().subtract(v * 7, 'd').format('YYYY-MM-DD')
-//   }))
-//   result = (await Promise.all(dates.map(d => query(term, d.start, d.end)))).reduce((acc, nxt) => [...acc, ...nxt])
-//   let ids = new Set()
-//   result.map(t => ids.add(t.id))
-//   res.header("Access-Control-Allow-Origin", "*")
-//   console.log(ids.size)
-//   res.send(result)
-// })
 
 app.listen(port, async () => {
   try {
