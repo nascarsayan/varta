cf82dafff326e77bf538b95a316d5dd2a6c951cc
Sayan Naskar <nascarsayan@iitkgp.ac.in>
2019-11-20 03:46:44 +0530

    Removed unrequired code, added db, cached results

---
+++ db/index.js
@@ -0,0 +1,52 @@
+const MongoClient = require('mongodb').MongoClient;
+class Database {
+
+  constructor(uri, dbname) {
+    this.uri = uri
+    this.dbname = dbname
+    this.db = {}
+    return this
+  }
+
+  async connect() {
+    MongoClient.connect(this.uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+      this.db = client.db(this.dbname)
+    });
+
+  }
+
+  async addTweets(tweets) {
+    await Promise.all(tweets.map(async tweet => {
+      const result = await this.db.collection('twstream').findOne({
+        'id_str': tweet.id_str
+      })
+      if (!result) {
+        this.db.collection('twstream').insertOne(tweet)
+      }
+    }))
+  }
+
+  async getTweets({ term, past }) {
+    let start = new Date()
+    start.setHours(0, 0, 0, 0)
+    start.setDate(start.getDate() - past)
+    const tweets = await this.db.collection('twstream').find({
+      created_on: { $gte: start },
+      term: { $regex: term, $options: 'i' }
+    }).toArray()
+    const cached = await this.isCached({ term })
+    return { tweets, cached }
+  }
+
+  async isCached({ term }) {
+    let start = new Date()
+    start.setHours(0, 0, 0, 0)
+    const tweets = await this.db.collection('twstream').find({
+      created_on: { $gte: start },
+      term: { $regex: term, $options: 'i' }
+    }).toArray()
+    return tweets.length > 0
+  }
+}
+
+module.exports = Database;
--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const Twitter = require('twitter')
 const express = require('express')
-const cors = require('cors')
+// const cors = require('cors')
+// const moment = require('moment')
+const queryString = require('query-string')
 require('dotenv').config()
+const Database = require('./db')
 
 const app = express()
 const port = 3002
-
+let db
 const client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -16,30 +19,105 @@ const client = new Twitter({
 if (process.env.PROXY === 'true') {
   client.request_options = { proxy: 'http://172.16.2.30:8080' }
 }
+
+let range = n => Array.from(Array(n).keys())
+
+const query = async (term, max_id = null) => {
+  try {
+    let qobj = { q: `place:b850c1bfd38f30e0 ${term}`, count: 100, include_entities: 1 }
+    if (max_id) {
+      qobj = { ...qobj, max_id }
+    }
+    // console.log(qobj)
+    let tweets = await client.get('search/tweets', qobj)
+    tweets.statuses = tweets.statuses.map(status => ({
+      ...status,
+      term,
+      date: new Date(status.created_at),
+      created_on: new Date()
+    }))
+    // console.log(tweets.statuses[0])
+    return tweets
+  } catch (err) {
+    console.log(err)
+    return {}
+  }
+}
+
 app.get('/', (req, res) => res.send('Hello World!!'))
 
-app.get('/api/query/recent', (req, res) => {
-  const term = req.param('term')
-  client.get('search/tweets', {
-    q: `place:b850c1bfd38f30e0 ${term}`,
-    count: 1000
-  },
-    function (error, tweets) {
-      res.header("Access-Control-Allow-Origin", "*")
-      console.log(tweets.statuses[0].entities.urls);
-      res.send(tweets.statuses.map(t => {
-        let url = null
-        try {
-          url = t.entities.urls[0].url
-        } catch {
-          url = 'https://twitter.com/'
-        }
-        return {
-          place: t.place.name, coord: t.place.bounding_box.coordinates, text: t.text, url
-        }
-      }));
-      // console.log(tweets);
-    })
+app.get('/api/query/recent', async (req, res) => {
+  const term = req.query.term
+  const past = parseInt(req.query.past || '7')
+  const iter = Math.min(parseInt(req.query.iter || '7'), 7)
+  let tres = []
+  try {
+    const { tweets, cached } = await db.getTweets({ term, past })
+    if (cached) {
+      tres = tweets
+    } else {
+      console.log('Fetching from twitter')
+      let max_id = null, ires
+      tres = []
+      for (let i = 0; i < iter; i++) {
+        ires = await query(term, max_id)
+        if (Object.keys(ires) === 0) break
+        tres = [...tres, ...ires.statuses]
+        // console.log(ires.search_metadata)
+        max_id = (queryString.parse(ires.search_metadata.next_results)).max_id
+        if (!max_id) break
+      }
+      await db.addTweets(tres)
+      console.log(`Fetched ${tres.length} queries of ${term}`)
+    }
+  } catch (err) {
+    console.log(err)
+  }
+  res.header("Access-Control-Allow-Origin", "*")
+  // console.log(tres.length)
+  res.send(tres)
 })
 
-app.listen(port, () => console.log(`App running on port ${port}`))
+// app.get('/api/query/max', async (req, res) => {
+//   const term = req.params('term')
+//   const query = async (term, start = null, end = null) => {
+//     // console.log(`place:b850c1bfd38f30e0 ${term}${(start) ? ` since:${start}` : ''}${(end) ? ` until:${end}` : ''}`);
+//     const tweets = await client.get('search/tweets', {
+//       q: `place:b850c1bfd38f30e0 ${term}${(start) ? ` since:${start}` : ''}${(end) ? ` until:${end}` : ''}`,
+//       count: 100
+//     })
+//     return tweets.statuses.map(t => {
+//       let url = null
+//       try {
+//         url = t.entities.urls[0].url
+//       } catch {
+//         url = 'https://twitter.com/'
+//       }
+//       return {
+//         id: t.id, place: t.place.name, coord: t.place.bounding_box.coordinates, text: t.text, url, raw: t
+//       }
+//     })
+//   }
+//   let range = n => Array.from(Array(n).keys())
+//   let result = [], old = 4
+//   let dates = range(old).map(v => ({
+//     start: moment().subtract((v + 1) * 7, 'd').format('YYYY-MM-DD'),
+//     end: moment().subtract(v * 7, 'd').format('YYYY-MM-DD')
+//   }))
+//   result = (await Promise.all(dates.map(d => query(term, d.start, d.end)))).reduce((acc, nxt) => [...acc, ...nxt])
+//   let ids = new Set()
+//   result.map(t => ids.add(t.id))
+//   res.header("Access-Control-Allow-Origin", "*")
+//   console.log(ids.size)
+//   res.send(result)
+// })
+
+app.listen(port, async () => {
+  try {
+    db = new Database(process.env.MONGO_URI || 'mongodb://localhost:27017', process.env.MONGO_DBNAME || 'twitter')
+    await db.connect()
+  } catch (err) {
+    console.log(err)
+  }
+  console.log(`App running on port ${port}`)
+})
